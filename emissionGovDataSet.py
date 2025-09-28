ed03987e981a35fa5a0f92337f87b1b78b95e8df
Arya Wiratama <aryawiratama99@gmail.com>
2025-09-28 16:58:58 +0800

    update: combine probrability from substance & industry

--- emissionGovDataSet.py
+++ emissionGovDataSet.py
@@ -2,15 +2,22 @@ import os
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from urllib.parse import quote_plus
+from datetime import datetime
 
 import webbrowser
-from datetime import datetime
 import pandas as pd
 
 import folium
 from folium.plugins import MarkerCluster
 
 import plotly.express as px
+
+#import statsmodels.api as sm
+#import statsmodels.formula.api as smf
+
+from sklearn.preprocessing import LabelEncoder
+from sklearn.linear_model import LogisticRegression
+
 
 load_dotenv()
 
@@ -20,14 +27,14 @@ db_pass = quote_plus(os.getenv("DB_PASS"))
 db_name = os.getenv("DB_NAME")
 
 fileTimeStamp = datetime.now().strftime('%Y%m%d_%H%M%S')
-output_dir = "/home/arya/Documents/Pasca Stikom/BigData/Repo/DataMap/mapRender/assets"
+output_dir = "/home/arya/Documents/Pasca Stikom/BigData/Repo/DataMap/tugas1BigData/assets"
 os.makedirs(output_dir, exist_ok=True) 
 
 conn = create_engine(f"mysql+mysqlconnector://{db_user}:{db_pass}@{db_host}/{db_name}")
 
-df = pd.read_sql("SELECT * FROM emmisions;", conn)
+df = pd.read_sql("SELECT * FROM emmisions ORDER BY RAND() LIMIT 300000;", conn)
 
-# distribution
+# ------------- distribution
 fig = px.histogram(
     df, 
     x="substance_name", 
@@ -48,16 +55,7 @@ fig.write_html(fileDistributionName, include_plotlyjs="cdn")
 print(f"\n✅ Finish Distribution Render → {fileDistributionName}\n")
 webbrowser.open(fileDistributionName)
 
-# scatter plot
-# fig_scatter = px.scatter(
-#     df,
-#     x="substance_name",
-#     y="report_year",
-#     color="substance_name",
-#     hover_data=["facility_name", "substance_name", 'report_year', "state"],
-#     title="Substance Count by Report Year",
-# )
-
+# ------------- group substance by year report
 df_grouped = df.groupby(
     ["report_year", "substance_name"]
 ).size().reset_index(name="count")
@@ -72,9 +70,9 @@ fig_scatter = px.scatter(
 )
 
 fig_scatter.update_layout(
-    xaxis_title="Substance",
+    xaxis_title="Substance Name",
     yaxis_title="Year",
-    legend_title="Report Year",
+    legend_title="Substance Name",
     template="plotly_white",
     yaxis=dict(
         tickmode="linear",
@@ -94,7 +92,74 @@ fig_scatter.write_html(fileScatterName, include_plotlyjs="cdn")
 print(f"\n✅ Finish Scatter Render → {fileScatterName}\n")
 webbrowser.open(fileScatterName)
 
-# map distribution
+# ------------- regret
+# drop empty rows
+df_reg = df.dropna(subset=['report_year', 'substance_name', 'primary_anzsic_class_name'])
+
+print(f"\n✅ Start cast data \n")
+# cast all data as a string
+df['report_year'] = df['report_year'].astype(str)
+df['substance_name'] = df['substance_name'].astype(str)
+df['primary_anzsic_class_name'] = df['primary_anzsic_class_name'].astype(str)
+
+X = pd.get_dummies(df['report_year'])
+
+print(f"\n✅ Start probs substance \n")
+le_substance = LabelEncoder()
+y_substance = le_substance.fit_transform(df['substance_name'])
+model_substance = LogisticRegression(solver='lbfgs', max_iter=500)
+model_substance.fit(X, y_substance)
+probs_substance = model_substance.predict_proba(X)
+probs_df_substance = pd.DataFrame(probs_substance, columns=le_substance.classes_)
+probs_df_substance['report_year'] = df['report_year'].values
+
+print(f"\n✅ Start probs industry \n")
+le_industry = LabelEncoder()
+y_industry = le_industry.fit_transform(df['primary_anzsic_class_name'])
+model_industry = LogisticRegression(solver='lbfgs', max_iter=500)
+model_industry.fit(X, y_industry)
+probs_industry = model_industry.predict_proba(X)
+probs_df_industry = pd.DataFrame(probs_industry, columns=le_industry.classes_)
+probs_df_industry['report_year'] = df['report_year'].values
+
+print(f"\n✅ Start concat \n")
+probs_df = pd.concat([probs_df_substance.drop(columns='report_year'),
+                      probs_df_industry.drop(columns='report_year')],
+                     axis=1)
+probs_df['report_year'] = df['report_year'].values
+
+# sortby year of report
+df = df.sort_values(by='report_year', ascending=True)
+
+# get top 10 substance
+top10_substance = df['substance_name'].value_counts().nlargest(10).index
+for sub in top10_substance:
+    df[f'prob_{sub}'] = probs_df[sub]
+
+# get top 10 industry
+top10_industry = df['primary_anzsic_class_name'].value_counts().nlargest(10).index
+for sub in top10_industry:
+    df[f'prob_{sub}'] = probs_df[sub]
+
+print(f"\n✅ Get Top 10 industry & substance \n")
+y_columns = [f'prob_{s}' for s in top10_substance] + [f'prob_{i}' for i in top10_industry]
+
+fig_combined_substance_industry = px.line(
+    df,
+    x='report_year',
+    y=y_columns,
+    title='Predicted Probability of Top 10 Substances & Top 10 Industries Over Years',
+    labels={'value':'Predicted Probability', 'report_year':'Year'}
+)
+
+file_combined_substance_industry_name = os.path.join(output_dir, f"regrt_combined_substance_industry_{fileTimeStamp}.html")
+fig_combined_substance_industry.write_html(file_combined_substance_industry_name, include_plotlyjs='cdn', full_html=True)
+
+print(f"\n✅ Finish Pobrality Render → {file_combined_substance_industry_name}\n")
+webbrowser.open(file_combined_substance_industry_name)
+
+
+# ------------- map distribution
 df = df.dropna(subset=['latitude','longitude'])
 df['latitude'] = df['latitude'].astype(float)
 df['longitude'] = df['longitude'].astype(float)
